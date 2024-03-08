5e91666b63320477adece5dd44292ea370812d53
jkmanait <jkmanait@gmail.com>
2024-03-08 08:30:55 +0800

    added card on tab1

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -17,8 +18,17 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         
-      </IonContent>
-    </IonPage>
+    <IonCard>
+      <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+           <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            </IonCardHeader>
+
+      <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+    </IonCard>
+        </IonContent>
+      </IonPage>
   );
 };
 
