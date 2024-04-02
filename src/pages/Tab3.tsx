6049a2fb0daa58bcb5f921947afd19ddd5f8261d
jkmanait <jkmanait@gmail.com>
2024-04-02 08:15:44 +0800

    added calculator code in tab3.tsx

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonGrid, IonRow, IonCol, IonInput } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value: string) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +34,57 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Calculator</IonTitle>
           </IonToolbar>
         </IonHeader>
-        
+        <IonGrid>
+          <IonRow>
+            <IonCol size="12">
+              <IonInput
+                value={input}
+                placeholder="Enter expression"
+                readonly
+                style={{ textAlign: 'right' }}
+              ></IonInput>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('7')}>7</IonButton>
+            </IonCol>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('8')}>8</IonButton>
+            </IonCol>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('9')}>9</IonButton>
+            </IonCol>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('/')}>/</IonButton>
+            </IonCol>
+          </IonRow>
+          {/* Add other rows of buttons here */}
+          <IonRow>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('C')}>C</IonButton>
+            </IonCol>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('0')}>0</IonButton>
+            </IonCol>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('=')}>=</IonButton>
+            </IonCol>
+            <IonCol size="3">
+              <IonButton onClick={() => handleClick('*')}>*</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        <IonRow>
+          <IonCol size="12">
+            <IonInput
+              value={result}
+              placeholder="Result"
+              readonly
+              style={{ textAlign: 'right', fontWeight: 'bold' }}
+            ></IonInput>
+          </IonCol>
+        </IonRow>
       </IonContent>
     </IonPage>
   );
