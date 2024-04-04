6e23f5c03454120aa785ebbcc679b1759177ed2a
jkmanait <jkmanait@gmail.com>
2024-04-05 07:51:59 +0800

    change tab2.tsx to click-counter.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import { calculatorOutline, ellipse, home, personOutline, speedometerOutline, sq
 import Home from './pages/home';
 
 import Profile from './pages/profile';
-import Tab2 from './pages/Tab2';
+import Tab2 from './pages/click-counter';
 import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
