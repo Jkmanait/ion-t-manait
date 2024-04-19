d1ffa1130eb03fd3d6d85c701b04988b74085ba3
jkmanait <jkmanait@gmail.com>
2024-04-19 08:25:30 +0800

    changed home content

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,38 +11,69 @@ import {
   IonCardTitle,
   IonCardContent,
   IonGrid,
-  IonRow,
   IonCol,
   IonRouterLink,
   IonIcon
 } from '@ionic/react';
-import { fingerPrintOutline, heartOutline } from 'ionicons/icons'; // Import the icon you want to use
+import { fingerPrintOutline, heartOutline, rocketOutline, thumbsUpOutline } from 'ionicons/icons'; // Import additional icons
 
 const CardExample: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Ionic Card Example</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
         <IonGrid>
-          <IonRow>
-            <IonCol size="6">
-              <IonRouterLink href="/card1">
-                <IonCard>
-                  <IonCardHeader>
-                    <IonIcon slot="start" icon={fingerPrintOutline} /> {/* Icon */}
-                    <IonCardSubtitle></IonCardSubtitle>
-                    <IonCardTitle>Click Counter</IonCardTitle>
-                  </IonCardHeader>
-                  
-                </IonCard>
-              </IonRouterLink>
-            </IonCol>
-            {/* Repeat for other cards */}
-          </IonRow>
+          <IonCol size="6">
+            <IonRouterLink href="/card1">
+              <IonCard>
+                <IonCardHeader>
+                  <IonIcon slot="start" icon={fingerPrintOutline} /> {/* Icon */}
+                  <IonCardSubtitle></IonCardSubtitle>
+                  <IonCardTitle className="ion-text-center">Click Counter</IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonRouterLink>
+          </IonCol>
+
+          <IonCol size="6">
+            <IonRouterLink href="/card2">
+              <IonCard>
+                <IonCardHeader>
+                  <IonIcon slot="start" icon={heartOutline} /> {/* Icon */}
+                  <IonCardSubtitle></IonCardSubtitle>
+                  <IonCardTitle className="ion-text-center">Calculator</IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonRouterLink>
+          </IonCol>
+
+          <IonCol size="6">
+            <IonRouterLink href="/card3">
+              <IonCard>
+                <IonCardHeader>
+                  <IonIcon slot="start" icon={rocketOutline} /> {/* Icon */}
+                  <IonCardSubtitle></IonCardSubtitle>
+                  <IonCardTitle className="ion-text-center">To Do List</IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonRouterLink>
+          </IonCol>
+
+          <IonCol size="6">
+            <IonRouterLink href="/card4">
+              <IonCard>
+                <IonCardHeader>
+                  <IonIcon slot="start" icon={thumbsUpOutline} /> {/* Icon */}
+                  <IonCardSubtitle></IonCardSubtitle>
+                  <IonCardTitle className="ion-text-center"></IonCardTitle>
+                </IonCardHeader>
+              </IonCard>
+            </IonRouterLink>
+          </IonCol>
         </IonGrid>
       </IonContent>
     </IonPage>
