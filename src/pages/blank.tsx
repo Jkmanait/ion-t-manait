76bcc0190c674fde1e170059b902b9f93d2cae6c
jkmanait <jkmanait@gmail.com>
2024-04-19 08:13:17 +0800

    make the blank.tsx a blank page

--- src/pages/blank.tsx
+++ src/pages/blank.tsx
@@ -1,9 +1,8 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonAlert, IonButton, IonActionSheet } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import './blank.css';
 
-const blank: React.FC = () => {
+const Blank: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
@@ -18,55 +17,18 @@ const blank: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         
-    <IonCard>
-      <img alt="Silhouette of mountains" src="../src/assets/img/avatar.PNG" />
-        <IonCardHeader>
-          <IonCardTitle>Manait, James Kenneth</IonCardTitle>
-           <IonCardSubtitle>Student</IonCardSubtitle>
-            </IonCardHeader>
-
-      <IonCardContent>Nothing more, nothing less.</IonCardContent>
-
-      <IonButton id="present-alert" expand="full" color="success">Quick Facts</IonButton>
-      <IonAlert
-        trigger="present-alert"
-        header="A Short Title Is Best"
-        subHeader="A Sub Header Is Optional"
-        message="A message should be a short, complete sentence."
-        buttons={['Action']}
-      ></IonAlert>
-
-<IonButton id="open-action-sheet" expand="block" color="medium">Open Action Sheet</IonButton>
-      <IonActionSheet
-        trigger="open-action-sheet"
-        header="Actions"
-        buttons={[
-          {
-            text: 'Delete',
-            role: 'destructive',
-            data: {
-              action: 'delete',
-            },
-          },
-          {
-            text: 'Share',
-            data: {
-              action: 'share',
-            },
-          },
-          {
-            text: 'Cancel',
-            role: 'cancel',
-            data: {
-              action: 'cancel',
-            },
-          },
-        ]}
-      ></IonActionSheet>
-    </IonCard>
-        </IonContent>
-      </IonPage>
+        <IonCard>
+          {/* <IonCardHeader>
+            <IonCardTitle>Manait, James Kenneth</IonCardTitle>
+            <IonCardSubtitle>Student</IonCardSubtitle>
+          </IonCardHeader> */}
+          <IonCardContent>
+            {/* Your content here */}
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
   );
 };
 
-export default blank;
+export default Blank;
