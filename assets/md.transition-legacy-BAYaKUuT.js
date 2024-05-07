746ca2e63aaf70aa168a2eb7bbde5b63cf35977f
jkmanait <jkmanait@gmail.com>
2024-05-07 08:36:52 +0800

    Updates

--- assets/md.transition-legacy-BAYaKUuT.js
+++ assets/md.transition-legacy-BAYaKUuT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DBzg8JUF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-DtI4am_6.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
