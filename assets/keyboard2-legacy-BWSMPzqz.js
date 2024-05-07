d681e2e58b44aecbe611e7e15a79072ebd255e5b
jkmanait <jkmanait@gmail.com>
2024-05-07 08:41:44 +0800

    Updates

--- assets/keyboard2-legacy-BWSMPzqz.js
+++ assets/keyboard2-legacy-BWSMPzqz.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DtI4am_6.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DQJ3SG8t.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
